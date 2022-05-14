2ddf49677169461e408694312c7167dbd08b8a2a
tomaszpankowski <--get-all>
2022-05-14 17:53:58 +0200

    frontend

--- src/app/product/sidebar/sidebar.component.ts
+++ src/app/product/sidebar/sidebar.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.sass']
 })
 export class SidebarComponent implements OnInit {
+  showFilters:boolean=true;
 
+  showHideFilters(){
+    this.showFilters=!this.showFilters;
+  }
   constructor() { }
 
   ngOnInit(): void {
